7bdca653e0f77d07d476b05b61368fb6fdacdc24
Javi Abia <weberjavi@gmail.com>
2018-12-04 16:35:56 +0100

    Add policies index and details sections

---
+++ app/javascript/app/router/sections/climate-policies.js
@@ -0,0 +1,10 @@
+export default [
+  {
+    slug: 'climate-policy-module',
+    label: 'Policy module',
+    description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit.',
+    path: '/climate-policies',
+    nav: false,
+    default: true
+  }
+];
---
+++ app/javascript/app/router/sections/climate-policy-detail.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    slug: 'overview',
+    label: 'Overview',
+    path: '/overview',
+    nav: true,
+    default: true
+  },
+  {
+    slug: 'instruments',
+    label: 'Instruments',
+    path: '/instruments',
+    nav: true,
+    default: false
+  },
+  {
+    slug: 'milestones',
+    label: 'Milestones',
+    path: '/milestones',
+    nav: true,
+    default: false
+  },
+  {
+    slug: 'indicators',
+    label: 'Indicators',
+    path: '/indicators',
+    nav: true,
+    default: false
+  },
+  {
+    slug: 'progress',
+    label: 'Snapshot of progress',
+    path: '/progress',
+    nav: true,
+    default: false
+  }
+];
