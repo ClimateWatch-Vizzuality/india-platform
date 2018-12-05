c4a099104d96ff2cadc224e1dc96b69c09745009
Javi Abia <weberjavi@gmail.com>
2018-12-05 10:35:57 +0100

    Add intro styles

--- app/javascript/app/pages/climate-policies/climate-policy-module/climate-policy-module-component.jsx
+++ app/javascript/app/pages/climate-policies/climate-policy-module/climate-policy-module-component.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import PoliciesCard from 'components/policies-card';
+import SearchCategoriesBox from 'components/search-categories-box';
 import { policiesList } from './climate-policies-mock';
 
 import styles from './climate-policy-module-styles';
 
 const ClimatePolicies = ({ title, description }) => (
   <div className={styles.pageLayout}>
-    <div className={styles.title}>{title}</div>
-    <div className={styles.description}>{description}</div>
+    <section className={styles.introAndSearch}>
+      <div className={styles.title}>{title}</div>
+      <div className={styles.contentWrapper}>
+        <div className={styles.content}>
+          <div className={styles.description}>{description}</div>
+          <SearchCategoriesBox
+            onChange={value => console.info(value)}
+            placeholder="Feel free to write me"
+          />
+        </div>
+      </div>
+    </section>
     {policiesList.map(policyType => (
       <section className={styles.policyTypeSection} key={policyType.name}>
         <h3 className={styles.title}>{policyType.name}</h3>
