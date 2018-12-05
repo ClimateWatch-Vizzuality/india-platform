5584e370ac00d276a58fbda09a715e06ba43522d
Simao Belchior <simaobelchior@gmail.com>
2018-12-05 09:23:50 +0000

    Updates schemas and annotations

--- db/schema.rb
+++ db/schema.rb
@@ -36,6 +36,19 @@ ActiveRecord::Schema.define(version: 2018_12_04_131137) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
+  create_table "admin_users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "role", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_admin_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+  end
+
   create_table "climate_policies", force: :cascade do |t|
     t.string "sector", null: false
     t.string "code", null: false
