4575ffe696b24dd16c33948368ba52ea2bf615ed
Javi Abia <weberjavi@gmail.com>
2018-12-12 12:50:51 +0100

    Delete unused onFilterChange

--- app/javascript/app/components/search-categories-box/search-categories-box.js
+++ app/javascript/app/components/search-categories-box/search-categories-box.js
@@ -1,23 +1,14 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { isEmpty } from 'lodash';
 
 import { searchSelectors } from './search-categories-box-selectors';
 import Component from './search-categories-box-component';
 import * as actions from './search-categories-box-actions';
 
 class SearchFilterContainer extends PureComponent {
-  onFilterChange = (filter, key) => {
-    const { updateFiltersSelected, query } = this.props;
-    if (isEmpty(filter[key])) {
-      delete query[key];
-    }
-    updateFiltersSelected({ query: { ...query, ...filter } });
-  };
-
   render() {
-    return <Component {...this.props} onFilterChange={this.onFilterChange} />;
+    return <Component {...this.props} />;
   }
 }
 
