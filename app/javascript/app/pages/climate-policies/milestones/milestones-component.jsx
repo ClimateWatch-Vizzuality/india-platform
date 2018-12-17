9b5204ea6d09dfc2b7ebf6d580d4fca0910f38f1
Marta Gajowczyk <gajowczyk.marta@gmail.com>
2018-12-17 14:11:45 +0000

    Polish milestones component

--- app/javascript/app/pages/climate-policies/milestones/milestones-component.jsx
+++ app/javascript/app/pages/climate-policies/milestones/milestones-component.jsx
@@ -1,26 +1,53 @@
 import React, { PureComponent } from 'react';
 import ReactMarkdown from 'react-markdown';
 import PropTypes from 'prop-types';
+import lowerCase from 'lodash/lowerCase';
+import capitalize from 'lodash/capitalize';
+import DateTime from 'luxon/src/datetime';
 import cx from 'classnames';
 import ClimatePolicyProvider from 'providers/climate-policy-provider';
 import { Icon } from 'cw-components';
-import iconInfo from 'assets/icons/info';
+
+import attained from 'assets/icons/attained';
+import issued from 'assets/icons/issued';
+import estimated from 'assets/icons/estimated';
 
 import styles from './milestones-styles';
 
-// const formatDate = date => DateTime.fromISO(date).toFormat('dd/M/yyyy');
-// const renderInfoIcon = () => <InfoButton dark slugs="" />;
+const ICONS = { attained, issued, estimated };
+
+const formatDate = date => DateTime.fromISO(date).toFormat('MMMM yyyy');
+
 const table = milestones => (
   <table className={styles.table}>
     <tbody>
       {milestones.map(milestone => (
-        <tr key={`${milestone.name}`} className={styles.row}>
+        <tr
+          key={`${milestone.name}-${milestone.data_source_link}`}
+          className={styles.row}
+        >
           <td className={cx(styles.statusColumn, styles.cell)}>
-            {milestone.status}
-            <Icon icon={iconInfo} theme={{ icon: cx(styles.statusIcon) }} />
+            <span className={styles.status}>
+              {capitalize(milestone.status)}
+            </span>
+            <Icon
+              icon={ICONS[lowerCase(milestone.status)]}
+              theme={{ icon: cx(styles.statusIcon) }}
+            />
           </td>
-          <td className={cx(styles.cell)}>
-            {milestone.name && <ReactMarkdown source={milestone.name} />}
+          <td className={cx(styles.cell, styles.description)}>
+            <p>{formatDate(milestone.date)}</p>
+            {
+              milestone.name &&
+                (
+                  <ReactMarkdown
+                    className={styles.name}
+                    source={milestone.name}
+                  />
+                )
+            }
+            <p>{milestone.responsible_authority}</p>
+            <a href={milestone.data_source_link}>Data source</a>
           </td>
         </tr>
       ))}
