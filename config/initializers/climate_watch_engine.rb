a002595c18df9cda28b44d6f48229f6b73796641
Tomasz Subik <tsubik@gmail.com>
2018-12-28 18:52:44 +0100

    import locations

--- config/initializers/climate_watch_engine.rb
+++ config/initializers/climate_watch_engine.rb
@@ -2,10 +2,12 @@ ClimateWatchEngine.s3_bucket_name = Rails.application.secrets.s3_bucket_name
 
 # Locations engine initializer
 require 'locations'
+Locations::LOCATION_TYPES = %w(COUNTRY STATE TERRITORY)
 Locations.locations_filepath = "#{CW_FILES_PREFIX}locations/locations.csv"
 Locations.cartodb_url =
   'https://wri-01.carto.com/api/v2/sql?q=SELECT%20name_engli,iso,topojson,centroid%20FROM%20gadm28_countries'
 Locations.location_groupings_filepath = "#{CW_FILES_PREFIX}locations/locations_groupings.csv"
+Locations.valid_location_types = %w(COUNTRY STATE TERRITORY)
 
 # HistoricalEmissions engine initializer
 require 'historical_emissions'
