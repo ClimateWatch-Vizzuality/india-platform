5b1b2fd0d50c69678db68cbfa74bbe3f1a67bf18
Tomasz Subik <tsubik@gmail.com>
2018-12-28 19:56:17 +0100

    import ghg emissions

---
+++ app/decorators/services/historical_emissions/import_historical_emissions_decorator.rb
@@ -0,0 +1,34 @@
+HistoricalEmissions::ImportHistoricalEmissions.class_eval do
+  headers metadata: [:source, :sector, :subsectorof],
+          records: [:iso_code3, :unit, :source, :sector, :gas]
+
+  def call
+    return unless all_headers_valid?
+
+    ActiveRecord::Base.transaction do
+      cleanup
+      import_sectors(meta_sectors_csv, HistoricalEmissions.meta_sectors_filepath)
+      import_records(data_cait_csv, HistoricalEmissions.data_cait_filepath)
+    end
+  end
+
+  def all_headers_valid?
+    [
+      valid_headers?(
+        meta_sectors_csv, HistoricalEmissions.meta_sectors_filepath, headers[:metadata]
+      ),
+      valid_headers?(data_cait_csv, HistoricalEmissions.data_cait_filepath, headers[:records])
+    ].all?(true)
+  end
+
+  def record_attributes(row)
+    {
+      location: Location.find_by(iso_code3: row[:iso_code3]),
+      data_source: HistoricalEmissions::DataSource.find_or_create_by!(name: row[:source]),
+      sector: HistoricalEmissions::Sector.find_by(name: row[:sector]),
+      gas: HistoricalEmissions::Gas.find_or_create_by!(name: row[:gas]),
+      gwp: HistoricalEmissions::Gwp.find_or_create_by!(name: 'AR2'),
+      emissions: emissions(row)
+    }
+  end
+end
--- lib/tasks/import.rake
+++ lib/tasks/import.rake
@@ -1,10 +1,8 @@
 namespace :db do
   desc 'Imports everything'
   task import: :environment do
-    # TODO: Files not ready yet in S3, importers not checked against actual data
     Rake::Task['locations:import'].invoke
-    # Rake::Task['location_members:import'].invoke
-    # Rake::Task['historical_emissions:import'].invoke
+    Rake::Task['historical_emissions:import'].invoke
     Rake::Task['climate_policies:import'].invoke
     puts 'All done!'
   end
