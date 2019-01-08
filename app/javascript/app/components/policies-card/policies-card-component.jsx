74e4cc6d856c4dda9daec3c92fca53035a6b98eb
Javi Abia <weberjavi@gmail.com>
2019-01-08 13:42:10 +0100

    Add library to truncate policies card text

--- app/javascript/app/components/policies-card/policies-card-component.jsx
+++ app/javascript/app/components/policies-card/policies-card-component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Link from 'redux-first-router-link';
+import Truncate from 'react-truncate-markup';
 
 import styles from './policies-card-styles';
 
@@ -10,7 +11,9 @@ const PoliciesCardComponent = (
   <Link className={styles.cardContainer} to={action}>
     <h3 className={styles.title}>{title}</h3>
     <h6 className={styles.responsibleAuthority}>{responsibleAuthority}</h6>
-    <p className={styles.description}>{description}</p>
+    <Truncate lines={3}>
+      <p className={styles.description}>{description}</p>
+    </Truncate>
   </Link>
 );
 
