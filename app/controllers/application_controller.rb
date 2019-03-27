f73c64b2abbaa3c1dc3ca82c5fa7256e0b12b2db
Simao Belchior <simaobelchior@gmail.com>
2019-03-27 14:24:45 +0100

    better way to check for development

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,12 +11,13 @@ class ApplicationController < ActionController::Base
   private
 
   def authorize_access
+    return true if Rails.env.development?
     if request.base_url == "https://india.climatewatchdata.org"
       authenticate_or_request_with_http_basic do |username, password|
         username == ENV['HTTP_AUTH_USERNAME'] &&
           password == ENV['HTTP_AUTH_PASSWORD']
       end
-    elsif request.base_url != "http://localhost:3000" && request.path != "/coming-soon"
+    elsif request.path != "/coming-soon"
       redirect_to "/coming-soon"
     else
       true
