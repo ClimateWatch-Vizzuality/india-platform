32d4af287ddc4f34978c6412bf3259de76b5d6a3
kfilip <kamil.filip94@gmail.com>
2019-04-04 14:12:01 +0100

    Change description and link of sponsor in footer

--- app/javascript/app/constants/founders.js
+++ app/javascript/app/constants/founders.js
@@ -2,9 +2,9 @@ import fmencbnsImage from 'assets/fmencbns';
 
 export default [
   {
-    link: 'http://www.bmub.bund.de/en/',
+    link: 'http://www.international-climate-initiative.com/en',
     orderingString: 'FMENCBNS',
     img: { alt: 'FMENCBNS', src: fmencbnsImage, customClass: 'BMULogo' },
-    description: 'This initiative is a part of the International Climate Initiative (IKI).'
+    description: 'Based on a decision of the German Bundestag'
   }
 ];
